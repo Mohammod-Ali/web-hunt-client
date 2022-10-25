55d234a3059decbf3de147c32b131285b75541e1
Mohammod Ali <mohammodali651@gmail.com>
2022-10-25 18:57:52 +0600

    nav icon added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FaMouse } from "react-icons/fa";
+
 
 const Header = () => {
     return (
-        <div>
-            this is header.
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"><FaMouse className='me-2'></FaMouse>WEB-<span className='fw-bold'>HUNT</span></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              
+            </Nav>
+            <Nav>
+                {FaMouse}
+              <Nav.Link to="/courses">Courses</Nav.Link>
+              <Nav.Link to="/faq">FAQ</Nav.Link>
+              <Nav.Link to="/blogs">Blogs</Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
