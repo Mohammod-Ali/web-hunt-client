e7294b1e79e1bfdcff719a38b3a2e0d38046eb45
Mohammod Ali <mohammodali651@gmail.com>
2022-10-25 23:24:09 +0600

    header added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 import { FaMouse } from "react-icons/fa";
 
-
 const Header = () => {
-    return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home"><FaMouse className='me-2'></FaMouse>WEB-<span className='fw-bold'>HUNT</span></Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              
-            </Nav>
-            <Nav>
-                {FaMouse}
-              <Nav.Link to="/courses">Courses</Nav.Link>
-              <Nav.Link to="/faq">FAQ</Nav.Link>
-              <Nav.Link to="/blogs">Blogs</Nav.Link>
-              
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar className="mb-3" collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/"><FaMouse className="me-2"></FaMouse> WEB-<span className="fw-bold">HUNT</span></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/courses">
+              Courses
+            </Nav.Link>
+            <Nav.Link as={Link} to="/faq">
+              FAQ
+            </Nav.Link>
+            <Nav.Link as={Link} to="/blogs">
+              Blogs
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
