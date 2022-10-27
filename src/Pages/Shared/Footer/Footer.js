8b6b25097839ab6c3a53d275fac2d0514d2ade80
Mohammod Ali <mohammodali651@gmail.com>
2022-10-27 22:03:41 +0600

    added footer content

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer style={{marginBottom: "0px", position: "relative", bottom: "0", top:"70px",  width:"100%"}} className="page-footer  blue pt-4 border ">
+    <footer className="page-footer  blue pt-4 border mt-3 mb-0">
       <div className="container-fluid text-center text-md-left">
         <div className="row">
           <div className="col-md-6 mt-md-0 mt-3">
             <h2 className="text-uppercase">Web-Hunt</h2>
             <p>
               Here you can learn Web Development and you can gain skills for job
-              market
+              market.
             </p>
+            <p><span className="fw-bold">Email:</span> fakemail@gmail.com</p>
+            <p><span className="fw-bold">Location:</span> Dhaka, Bangladesh.</p>
+            <div className="fs-2"><FaFacebook className="me-3"></FaFacebook> 
+            <FaLinkedin></FaLinkedin></div>
           </div>
 
           <hr className=" w-100 d-md-none pb-0" />
